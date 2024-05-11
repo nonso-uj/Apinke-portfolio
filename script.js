79600b8575d21b487c376a45d756b27dfe9aef02
nonso-uj <nonso.udonne@gmail.com>
2024-05-11 05:04:33 +0100

    2nd

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ function toggleMenu() {
     icon.classList.toggle("open");
   }
   
+
+document.addEventListener("DOMContentLoaded", function() {
+  const showMoreBtn = document.getElementById("showMoreBtn");
+  const moreContent = document.getElementById("moreContent");
+
+  showMoreBtn.addEventListener("click", function() {
+    console.log('bjdcjdsjhvhsjbdh')
+      moreContent.classList.toggle("hidden");
+      if (moreContent.classList.contains("hidden")) {
+          showMoreBtn.textContent = "SHOW MORE";
+      } else {
+          showMoreBtn.textContent = "SHOW LESS";
+      }
+  });
+});
